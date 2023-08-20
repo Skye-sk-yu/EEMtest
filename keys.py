6b3e901cdc02b3163594fb31a9c040d426a84e55
Skye-sk-yu <141044975+Skye-sk-yu@users.noreply.github.com>
2023-08-20 20:44:00 +0800

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,3 +1,3 @@
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = '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'
 PINECONE_API_KEY = '8eaea7f2-4fa2-4202-8f86-bda60c6f627f'
 PINECONE_API_ENV = 'asia-southeast1-gcp-free'
